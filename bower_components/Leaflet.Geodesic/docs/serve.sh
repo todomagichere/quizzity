61cd4225f5ead25eff2e540803dc5f2c492223a1
Roberto Pérez <rperez@infinitumdigital.net>
2022-11-24 18:19:23 +0100

    pray

--- bower_components/Leaflet.Geodesic/docs/serve.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# This creates a webserver within the current folder. Used for local testing.
-python3 -m http.server 8082
--- bower_components/Leaflet.Geodesic/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  coverageReporters: ["json", "lcov", "text", "clover"],
-  testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
-  coveragePathIgnorePatterns: ["<rootDir>/spec/"]
-};
